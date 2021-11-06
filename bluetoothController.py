8d1e9f36f296acc30e656f60b295f0e29ece0bf2
KubaBoi <jakubanderle@outlook.cz>
2021-11-07 00:36:54 +0100

    debug

--- bluetoothController.py
+++ bluetoothController.py
@@ -29,6 +29,8 @@ class BluetoothController:
             if not self.transport_prop_iface:
                 print("Error: DBus.Properties iface not found.")
 
+        print(self.transport_prop_iface.GetAll())
+
         bus.add_signal_receiver(
                 self.on_property_changed,
                 bus_name="org.bluez",
