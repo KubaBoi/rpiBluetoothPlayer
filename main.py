e6ebe4cb3981c3433d0ea40c81512c93a7707cad
KubaBoi <jakubanderle@outlook.cz>
2021-11-06 18:29:24 +0100

    debug

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import threading
 from bluetoothController import BluetoothController
 
 bController = BluetoothController()
-thread = threading.Thread(target=bController.serveForever, args=())
-thread.start()
+bController.serveForever()
+#thread = threading.Thread(target=bController.serveForever, args=())
+#thread.start()
 
 while True:
     pass
