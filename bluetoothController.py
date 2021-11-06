8d8da097dbdf438164ecd9a9eeacbe5792fa24c7
KubaBoi <jakubanderle@outlook.cz>
2021-11-07 00:26:06 +0100

    debug

--- bluetoothController.py
+++ bluetoothController.py
@@ -92,7 +92,7 @@ class BluetoothController:
         BT_Media_iface = dbus.Interface(player, dbus_interface='org.bluez.MediaPlayer1')
         BT_Media_props = dbus.Interface(player, "org.freedesktop.DBus.Properties")
 
-        props = BT_Media_iface.GetAll("org.bluez.MediaPlayer1") 
+        props = player.GetAll("org.bluez.MediaPlayer1") 
         return props
 
     def play(self):
