85ae2bd8d7b297be45babef60d01a83e723216a6
KubaBoi <jakubanderle@outlook.cz>
2021-11-06 23:29:07 +0100

    debug

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import threading
 import webbrowser
 import time
 import os
-import pyautogui
 
 from bluetoothController import BluetoothController
 from player import BluePlayer
@@ -21,7 +20,7 @@ server = HTTPServer(("localhost", 8000), Server)
 thread2 = threading.Thread(target=server.serve_forever, args=())
 thread2.start()
 
-
+import pyautogui
 webbrowser.open("http://localhost:8000")
 time.sleep(10)
 pyautogui.press("F11")
