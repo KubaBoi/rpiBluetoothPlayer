aae011edc87def78b2c940f9b81801e040ce0c69
KubaBoi <jakubanderle@outlook.cz>
2021-11-06 18:33:39 +0100

    debug

--- bluetoothController.py
+++ bluetoothController.py
@@ -44,7 +44,7 @@ class BluetoothController:
             
             if prop == "Status":
                 self.data["Status"] = value
-                print(f"Playback Status: {value}")
+                print(f"Playback Status: {str(value)}")
 
             elif prop == "Track":
                 print("Music Info:")
