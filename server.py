ca4c159c105b63792fca688a5093d6d15715f0dd
KubaBoi <jakubanderle@outlook.cz>
2021-11-06 18:55:38 +0100

    debug

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ from os import path, listdir
 import pathlib
 import datetime
 import json
+import threading
+
+from bluetoothController import BluetoothController
 
 hostName = "localhost"
 hostPort = 8000
@@ -17,11 +20,8 @@ class Server(BaseHTTPRequestHandler):
         elif (self.path == "/update"):
             self.update()
 
-        else:
-            self.sendFile(self.path[1:])
-
     def do_POST(self):
-        if (self.path == "/message"):
+        if (self.path == "/update"):
             content_len = int(self.headers.get('Content-Length'))
             post_body = self.rfile.read(content_len)
             self.sendMessage(post_body)
@@ -40,6 +40,14 @@ class Server(BaseHTTPRequestHandler):
         with open(file, "rb") as f:
             self.wfile.write(f.read())
 
+    def update(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/json")
+        self.end_headers()
+
+        with open("data.json", "rb") as f:
+            self.wfile.write(f.read())
+
 
 
 
@@ -47,6 +55,10 @@ myServer = HTTPServer((hostName, hostPort), Server)
 print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
 
 try:
+    bController = BluetoothController()
+    thread = threading.Thread(target=bController.serveForever, args=())
+    thread.start()
+
     myServer.serve_forever()
 except KeyboardInterrupt:
     pass
