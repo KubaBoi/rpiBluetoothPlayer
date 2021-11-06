cc92cf34bcd6b4086fee19ffad54b993fa93b5b2
KubaBoi <jakubanderle@outlook.cz>
2021-11-07 00:32:05 +0100

    debug

--- main.py
+++ main.py
@@ -3,18 +3,27 @@ import threading
 import webbrowser
 import time
 import os
+import dbus
 
 from bluetoothController import BluetoothController
 from server import Server
 
-bController = BluetoothController()
+bus = dbus.SystemBus()
+
+player = bus.get_object('org.bluez','/org/bluez/hci0/dev_78_6A_89_FA_1C_95/player0')
+BT_Media_iface = dbus.Interface(player, dbus_interface='org.bluez.MediaPlayer1')
+BT_Media_props = dbus.Interface(player, "org.freedesktop.DBus.Properties")
+
+props = BT_Media_props.GetAll("org.bluez.MediaPlayer1") 
+
+"""bController = BluetoothController()
 thread = threading.Thread(target=bController.serveForever, args=())
 thread.start()
 
 server = HTTPServer(("localhost", 8000), Server)
 thread2 = threading.Thread(target=server.serve_forever, args=())
 thread2.start()
-
+"""
 """import pyautogui
 webbrowser.open("http://localhost:8000")
 time.sleep(10)
