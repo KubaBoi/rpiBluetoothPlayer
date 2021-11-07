9da4816685447d1d42ebd84ac1e7a95ea738a1f9
KubaBoi <jakubanderle@outlook.cz>
2021-11-07 01:23:02 +0100

    debug

--- server.py
+++ server.py
@@ -29,6 +29,7 @@ class Server(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/html")
             self.end_headers()
+            self.wfile.write(b":(")
             return
 
         self.send_response(200)
